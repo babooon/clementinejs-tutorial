66e7e4a698129b3dea70aaff7a549a786f9934bd
babooon <petramueller30@gmx.de>
2016-11-21 15:18:25 +0000

    basic tutorial

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require('express');
+var routes = require('./app/routes/index.js');
+var app = express();
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+
+    if (err){ throw new Error('Database failed to connect!'); }
+    else{ console.log('MongoDB successfully connected on port 27017.'); }
+    
+    app.use('/public', express.static(process.cwd() + '/public'));
+
+    app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+    
+    routes(app);
+    
+    app.listen(8080, function () {
+        console.log('Listening on port 8080...');
+    });
+    
+});
